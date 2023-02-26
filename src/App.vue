deae9bf15a41fe2ad84ebdc21ec4abc68e13cae2
Andrei <andrei.hudalla@outlook.com>
2023-02-26 12:11:11 +0300

    Site preview

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,16 @@
 <script setup>
-
+import TeamLogo from './components/TeamLogo.vue';
 </script>
 
 <template>
-  <!---- Header ---->
-  <header>
-    
-  </header>
-  <!---- Main ---->
-  <main>
-
-  </main>
+	<!---- Header ---->
+	<header>
+		<TeamLogo />
+	</header>
+	<!---- Main ---->
+	<main>
+		<h1>👀</h1>
+	</main>
 </template>
 
 <style scoped>
---
+++ src/components/TeamLogo.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+    <img src="/images/dap_logo.webp" alt="Logo">
+</template>
+
+<style scoped>
+    img {
+        width: 4rem;
+    }
+</style>
